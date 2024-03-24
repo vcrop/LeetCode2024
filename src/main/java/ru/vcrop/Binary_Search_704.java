1c0e535ede2fc072b07938c19bc3ac3d583fb4d3
vcrop <viktor.raguev@gmail.com>
2024-03-24 13:15:53 +0300

    binary search study plann

--- src/main/java/ru/vcrop/Binary_Search_704.java
+++ src/main/java/ru/vcrop/Binary_Search_704.java
@@ -1,2 +1,17 @@
-package ru.vcrop;public class Binary_Search_704 {
+package ru.vcrop;
+
+//https://leetcode.com/problems/binary-search/
+//Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Search.
+
+public class Binary_Search_704 {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int middle = (right - left) / 2 + left;
+            if (nums[middle] < target) left = middle + 1;
+            else right = middle;
+        }
+        return nums[left] == target ? left : -1;
+    }
+
 }
